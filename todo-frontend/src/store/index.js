4d1856004db6fcccd7bac35e3fae4d95d511bcf8
Fakultet Organizacionih Nauka <student1@prod.fon.bg.ac.rs>
2019-12-11 11:43:33 +0100

    aaa

--- todo-frontend/src/store/index.js
+++ todo-frontend/src/store/index.js
@@ -5,6 +5,7 @@ Vue.use(Vuex);
 // Vuex je centralizovano skladiste state-a nase aplikacije, kao npr. Redux
 // U ovom primeru imamo samo baseUrl, koji setujemo kako bismo iz cele aplikacije
 // znali na kojoj adresi nam je backend server
+// dasdasdasas
 export default new Vuex.Store({
   state: {
     baseUrl: 'http://localhost:8080/todofullstack/todo-backend'
